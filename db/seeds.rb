50f82a97a3ffcf256a4641921fb71bb11163e592
Micah Sterling <msterling89@gmail.com>
2021-02-28 09:40:09 -0600

    added some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# shoe = Shoe.new(brand: "micster", style: "toothy", color: "beam blue", size: "5-10", stock: "yes")
+# shoe.save
+shoe = Shoe.new(brand: "micster", style: "bommer", color: "black blue", size: "5-10", stock: "yes")
+shoe.save
+hoe = Shoe.new(brand: "micster", style: "circle M", color: "Yellow", size: "5-10", stock: "yes")
+shoe.save
+shoe = Shoe.new(brand: "micster", style: "curious", color: "ruby red", size: "5-10", stock: "yes")
+shoe.save
